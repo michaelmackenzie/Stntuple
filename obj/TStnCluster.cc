9619a4887c7ee1d4fc1d9ec08d4eba2af71c837c
michaelmackenzie <michaelmackenzie@u.northwestern.edu>
2026-04-19 14:10:43 -0500

    Add verbose cluster printout with crystal hits

--- obj/TStnCluster.cc
+++ obj/TStnCluster.cc
@@ -220,21 +220,27 @@ void TStnCluster::Print(Option_t* Option) const {
 					// "non-const *this" for printing purposes
   TStnCluster* cl = (TStnCluster*) this;
 
-  if ((opt == "") || (opt.Index("banner") >= 0)) {
+  if ((opt == "") || (opt.Index("banner") >= 0) || (opt == "v")) {
 //-----------------------------------------------------------------------------
 // print banner
 //-----------------------------------------------------------------------------
-    printf("-------------------------------------------------------\n");
-    printf("  i  disk ncr energy    time    X    Y    Z    nX    nY\n");
-    printf("-------------------------------------------------------\n");
+    printf("-------------------------------------------------------------------------\n");
+    printf("   i disk ncr  energy     time        X       Y       Z       nX       nY\n");
+    printf("-------------------------------------------------------------------------\n");
   }
 
-  if ((opt == "") || (opt.Index("data") >= 0)) {
-    printf("%4i %3i %3i %8.3f %8.3f ",
+  if ((opt == "") || (opt.Index("data") >= 0) || opt.Contains("v")) {
+    printf("%4i %4i %3i %7.2f %8.1f ",
 	   fNumber,cl->DiskID(), cl->fNCrystals, cl->fEnergy, cl->fTime);
-    printf(" %8.3f %8.3f %8.3f %8.3f %8.3f",
+    printf(" %7.1f %7.1f %7.3f %8.3f %8.3f",
 	   cl->fX, cl->fY, cl->fZ, cl->fNx, cl->fNy);
     printf("\n");
+  }
+  if(opt.Contains("v")) { // print each crystal's info
+    for(int crystal = 0; crystal < NCrystals(); ++crystal) {
+      printf("  Crystal %2i: ID = %4i, E = %5.2f, T = %8.2f\n",
+             crystal, CrystalID(crystal), CrystalE(crystal), CrystalT(crystal));
+    }
   }
 }
 
